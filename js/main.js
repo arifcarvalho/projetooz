0395e038cef4784efe416f5780ae9e332d884d45
arifcarvalho <carvalhoarif@gmail.com>
2021-11-21 19:25:46 -0300

    progresso

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var btnContact = document.querySelector('.oz-btn-contact');
+
+
+btnContact.addEventListener('click', function () {
+ var boxContact = document.querySelector('.oz-contact-info');
+
+ boxContact.classList.toggle('oz-is-open');
+
+});
